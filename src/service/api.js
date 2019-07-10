7003346e2b65fa67a9b2efd008efe751a091a712
Arthur Lima <arthur-esc@hotmail.com>
2019-07-09 23:16:25 -0300

    Removido busca por timezone...

--- src/service/api.js
+++ src/service/api.js
@@ -2,7 +2,6 @@ import axios from "axios";
 
 const api = axios.create({
   headers: {
-    "Time-Zone": "America/Sao_Paulo",
     Accept: "application/vnd.github.cloak-preview"
   },
   baseURL: "https://api.github.com"
